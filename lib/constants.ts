b175710844d8a60c0f4d6aba86074e56352d183b
Mulinda Yahaya <mymyahaya@gmail.com>
2025-02-14 22:57:59 +0300

    Added Home Page layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn2025'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Next Amazona 2025 is a full-stack e-commerce application built with Next.js, TypeScript, Tailwind CSS, and MongoDB.'
